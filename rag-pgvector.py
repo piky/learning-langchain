6e5b2d9ec66231f4472e013122caee3efa1512dc
Wijai Thongsom <wijai.thongsom@gmail.com>
2025-08-04 06:20:24 +0000

    Test embedding using BAAI/bge-m3

---
+++ rag-pgvector.py
@@ -0,0 +1,58 @@
+from os import getenv
+import psycopg2
+from langchain_openai.chat_models import ChatOpenAI
+from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = psycopg2.connect(
+    dbname="mydb",
+    user="postgres",
+    password=getenv("POSTGRES_PASSWORD"),
+    host="localhost",
+    port="5432"
+)
+
+# cur = conn.cursor()
+# cur.execute("""
+#     CREATE TABLE IF NOT EXISTS documents (
+#         id SERIAL PRIMARY KEY,
+#         content TEXT,
+#         embedding vector(1536)
+#     )
+# """)
+# conn.commit()
+# cur.close()
+# conn.close()
+
+# prompt = """Who is the Prime Minister of Thailand?"""
+
+# llm = ollama.chat(
+#     model = "scb10x/llama3.2-typhoon2-1b-instruct",
+#     messages = [{"role": "user", "content": prompt}]
+#     )
+
+# llm2 = ChatOpenAI(
+#     openai_api_key = getenv("OPENROUTER_API_KEY"),
+#     openai_api_base = getenv("OPENROUTER_BASE_URL"),
+#     model_name="moonshotai/kimi-k2:free",
+# )
+
+# print(llm2.invoke(prompt).content)
+# cur = conn.cursor()
+# cur.execute("""
+#             CREATE TABLE IF NOT EXISTS documents (
+#                 id SERIAL PRIMARY KEY,
+#                 content TEXT,
+#                 embedding vector(1536)
+#             )
+# """)
+
+# conn.commit()
+
+embedder = SentenceTransformer("BAAI/bge-m3")
+embedding = embedder.encode("วิทยาลัยนวัตกรรมดิจิทัลเทคโนโลยี มหาวิทยาลัยรังสิตเปิดหลักสูตรปริญญาตรี 5 สาขา").tolist()
+
+print(embedding)
+print(len(embedding))
